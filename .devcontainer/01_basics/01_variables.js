db622933e392c52c839a5adbe1937945ec383605
Suraj Kumar Sharma <139531472+suraj87899@users.noreply.github.com>
2025-01-30 19:53:01 +0000

    dataType

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "surajsharma87899@gamil.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accoutId =2//not allowed
+accountEmail="hc@hc.com"
+accountPassword ="21212121"
+accountCity="Bangaluru"
+
+console.log(accountId);
+/*
+prefer not to use  var
+because of issue in block scope and functionnal scope
+*/
+console.table([accountEmail,accountPassword,accountCity,accountState])
